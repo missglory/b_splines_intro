880d563abd7d20ab51b4bacdd69ae61be3b32b40
missglory <xhaustful@gmail.com>
2019-04-26 18:14:57 +0300

    1d

--- main.cpp
+++ main.cpp
@@ -55,6 +55,16 @@ cv::Point2f ComputePoint(const cv::Mat& Nx, const cv::Mat& Ny, const std::vector
 	return ret;
 }
 
+cv::Point2f ComputePoint(const cv::Mat& Nx, const std::vector<cv::Point2f>& controls, int x, int tn)
+{
+    cv::Point2f ret;
+    for (int i = 0; i < tn; i++) {
+        ret += Nx.at<float>(i, x) * controls[i];
+    }
+    return ret;
+}
+
+
 void mouse_callback(int event, int x, int y, int flags, void* userdata)
 {
 	static int activeCP = -1;
@@ -83,17 +93,20 @@ void mouse_callback(int event, int x, int y, int flags, void* userdata)
 		}
 	}
 	if (event == cv::EVENT_MOUSEMOVE && flags == cv::EVENT_FLAG_LBUTTON) {
-		if (activeCP >= 0) {
+        if (activeCP >= -1) {
 			controls[activeCP] = currentP;
 			orig.copyTo(image);
 
-			for (int i = 0; i < h; i++) {
+            int tn = 100;
+            int n = 14;
+            for (int i = 0; i < tn; i++) {
 				cv::Vec3b *p_im = image.ptr<cv::Vec3b>(i);
-				for (int j = 0; j < w; j++) {
-					cv::Point2f new_coord = ComputePoint(Nx, Ny, controls, j, i);
-					cv::Point2f uv_coord = 2 * cv::Point2f(j, i) - new_coord;
-					p_im[j] = BilinInterp(orig, uv_coord.x, uv_coord.y);
-				}
+//				for (int j = 0; j < w; j++) {
+                    cv::Point2f new_coord = ComputePoint(Nx, controls, i, n);
+                    cv::circle(image, new_coord, 3, cv::Scalar(10,250,10), -1);
+//					cv::Point2f uv_coord = 2 * cv::Point2f(j, i) - new_coord;
+//					p_im[j] = BilinInterp(orig, uv_coord.x, uv_coord.y);
+//				}
 			}
 
 			for (auto &pt : controls) {
@@ -120,27 +133,53 @@ int main()
 		{0.f,h-1},			{w / 3,h-1},		{2 * w / 3,h-1},		{w-1,h-1}
 	};
 
-	
+    int n = 14;
+    controls.resize(/*3 * */n);
+    cv::Point2f center(w/2, h/2);
+    for (int i = 0; i < n; i++)
+    {
+        float amp = 100;
+        float amp2 = 1.4;
+        float angle = (float)i / n * 2 * 3.14;
+        cv::Point2f vec(amp * cos(angle), amp * sin(angle));
+//        cv::circle(image, vec + center, 5, cv::Scalar(10, 10,250), 10);
+        controls[i] = vec + center;
+//        vec *= amp2;
+//        controls[n + i] = vec + center;
+//        vec *= amp2;
+//        controls[2 * n + i] = vec + center;
+
+    }
+
+
 	const int control_num = 4;
 	const int q = 4;
-	std::vector<float> knots = { 0.f, 0.f, 0.f, 0.f, 1.0f, 1.f, 1.f, 1.f };
-	std::vector<float> knots_x(knots.size()), knots_y(knots.size());
+//	std::vector<float> knots = { 0.f, 0.f, 0.f, 0.f, 1.0f, 1.f, 1.f, 1.f };
+    std::vector<float> knots =
+    {
+        0.0, 1.0 / 18, 2.0 / 18, 3.0 / 18, 4.0 / 18, 5.0 / 18,
+        6.0 / 18, 7.0/ 18, 8.0 / 18, 9.0 / 18, 10.0 / 18, 11.0 / 18,
+        12.0/18, 13./18, 14./18, 15./18, 16./18, 17./18, 1.
+    };
+    int tn = 100;
+    std::vector<float> knots_x(knots.size()), knots_y(knots.size());
 	for (size_t i = 0; i < knots.size(); i++) {
-		knots_x[i] = w * knots[i];
+        knots_x[i] = tn * knots[i];
 		knots_y[i] = h * knots[i];
 	}
 
 
-	cv::Mat Nfunc_x(control_num, w, CV_32FC1), Nfunc_y(control_num, h, CV_32FC1);
 
-	for (int n = 0; n < control_num; n++) {
-		for (int t = 0; t < w; t++) {
-			Nfunc_x.at<float>(n, t) = N(knots_x, t, n, q);
+    cv::Mat Nfunc_x(n, tn, CV_32FC1);//, Nfunc_y(control_num, h, CV_32FC1);
+
+    for (int i = 0; i < n; i++) {
+        for (int t = 0; t < tn; t++) {
+            Nfunc_x.at<float>(i, t) = N(knots_x, t, i, q);
 		}
 
-		for (int t = 0; t < h; t++) {
-			Nfunc_y.at<float>(n, t) = N(knots_y, t, n, q);
-		}
+//		for (int t = 0; t < h; t++) {
+//			Nfunc_y.at<float>(n, t) = N(knots_y, t, n, q);
+//		}
 	}
 
 	for (auto &pt : controls) {
@@ -152,10 +191,13 @@ int main()
 	ud.image	= image;
 	ud.orig		= orig;
 	ud.Nx		= Nfunc_x;
-	ud.Ny		= Nfunc_y;
+//	ud.Ny		= Nfunc_y;
 	ud.controls = controls;
 	ud.window_name = name;
 	
+//    cv::namedWindow("n", cv::WINDOW_FREERATIO);
+//    cv::imshow("n", Nfunc_x);
+
 	cv::namedWindow(name);
 	cv::setMouseCallback(name, mouse_callback, &ud);
 	cv::imshow(name, image);
